4207494cc2b44bc7e22137c2d9de77893c681ce7
hasonda <%eQ?DW5Js$,y96%>
2023-05-30 17:42:12 +0500

    hrportal2

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://1532773-cx56354.twc1.net/api/v1/'
+export const API_URL = https://1532773-cx56354.twc1.net/api/v1/'
 const $api = axios.create({
     // withCredentials: true,
     baseURL: API_URL
@@ -12,4 +12,4 @@ $api.interceptors.request.use(config => {
     return config;
 })
 
-export default $api;
+export default $api
